5baa665b7046cc6b3ff0b3f45903252207524032
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2024-11-23 19:33:12 +0700

    update portfolio and publick api 1.0.0

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,60 @@
-import express, { Request, Response } from 'express'; 
-import path from 'path';
+import express, { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 
-// Middleware untuk mengakses semua file statis di dalam folder 'public'
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));  // Menjadikan folder public sebagai root file statis
+app.use(express.static(path.join(__dirname, "../public")));
 
-// Endpoint utama untuk mengirim file index.html
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-// Menjalankan server di port 3000
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.get("/api/", (req: Request, res: Response) => {
+  res.redirect("/api/docs");
+});
+app.get("/api/docs", (req: Request, res: Response) => {
+  const filePath = path.resolve(__dirname, "../data/documentation.json");
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send({ message: "Error reading documentation file" });
+    } else res.json(JSON.parse(data));
+  });
+});
+
+app.get("/api/quote-on-the-day", (req: Request, res: Response) => {
+  const filePath = path.resolve(__dirname, "../data/data.json");
+  console.log("File path for quote-on-the-day:", filePath);
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ status: "error", message: "Unable to read data.json" });
+    }
+    const quotes = JSON.parse(data);
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.json({
+      status: "success",
+      data: randomQuote,
+    });
+  });
+});
+
+app.get("/api/quotes", (req: Request, res: Response) => {
+  const filePath = path.resolve(__dirname, "../data/data.json");
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ status: "error", message: "Unable to read data.json" });
+    }
+    const quotes = JSON.parse(data);
+    res.json({
+      status: "success",
+      data: quotes,
+    });
+  });
 });
 
 export default app;
