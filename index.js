babf5ab86104c7a2cb148d772c171341cb7759dc
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-01-22 19:09:31 +0700

    CHORE: connect to  supabase db

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const SUPABASE_URL = process.env.SUPABASE_URL; 
 const SUPABASE_KEY = process.env.SUPABASE_KEY; 
