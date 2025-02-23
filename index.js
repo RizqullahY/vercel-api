8f83f12ec801dbd67981f2705a04d7bb272395f5
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-02-23 11:04:14 +0700

    CHORE: add cache pac from 3d web, update index and vercel.json

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ import path from 'path';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
-import { createClient } from '@supabase/supabase-js';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL; 
-const SUPABASE_KEY = process.env.SUPABASE_KEY; 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -43,7 +41,7 @@ app.get("/api/", (req, res) => {
 });
 
 app.get("/api/quote-of-the-day", (req, res) => {
-  const filePath = path.resolve(__dirname, "./data/data.json");
+  const filePath = path.join(process.cwd(), "data", "data.json");
   console.log("File path for quote-on-the-day:", filePath);
   fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) {
@@ -76,69 +74,10 @@ app.get("/api/quotes", (req, res) => {
   });
 });
 
-// Endpoint GET /api/animated-control
-app.get('/api/animated-control', async (req, res) => {
-  try {
-    const { data, error } = await supabase
-      .from('animated_status')
-      .select('*')
-      .single();
+export default app;
 
-    if (error) {
-      throw error;
-    }
-
-    res.json({
-      status: 'success',
-      data,
-    });
-  } catch (error) {
-    console.error('Error fetching animated status:', error);
-    res.status(500).json({
-      status: 'error',
-      message: 'Unable to fetch animated status.',
-    });
-  }
-});
-
-// Endpoint POST /api/animated-control
-app.post('/api/animated-control', async (req, res) => {
-  try {
-    const { data: currentStatus, error: fetchError } = await supabase
-      .from('animated_status')
-      .select('*')
-      .single();
-
-    if (fetchError) {
-      throw fetchError;
-    }
-
-    const newStatus = !currentStatus.animated;
-
-    const { data, error: updateError } = await supabase
-      .from('animated_status')
-      .update({ animated: newStatus })
-      .eq('id', currentStatus.id);
-
-    if (updateError) {
-      throw updateError;
-    }
-
-    res.json({
-      status: 'success',
-      message: 'Status updated successfully.',
-      data,
-    });
-  } catch (error) {
-    console.error('Error updating animated status:', error);
-    res.status(500).json({
-      status: 'error',
-      message: 'Unable to update animated status.',
-    });
-  }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// FOR DEV
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
